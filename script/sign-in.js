334240897f00a605530dac0fcdcb8579cf69c4a6
nahidhasan330874 <nahidhasan1005004@gmail.com>
2026-03-09 21:47:28 +0600

    added the home page html

--- script/sign-in.js
+++ script/sign-in.js
@@ -1,1 +1,19 @@
 
+document.getElementById('signin-btn').addEventListener(('click'), () => {
+  
+    const inputName = document.getElementById('input-name');
+     const userName = inputName.value;
+     console.log(userName)
+
+    const inputPin = document.getElementById('input-pin');
+     const pin = inputPin.value;
+     console.log(pin)
+
+     if( userName == "admin" && pin == "admin123"){
+        alert(" Sing In Successful");
+        window.location.assign("home.html");
+     }else{
+        alert("Sing In Failed");
+        return;
+     }
+})
