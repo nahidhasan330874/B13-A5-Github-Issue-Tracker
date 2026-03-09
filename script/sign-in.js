fc147aaafb8acdddef7c4055f3f6396d01dd110c
nahidhasan330874 <nahidhasan1005004@gmail.com>
2026-03-09 22:02:06 +0600

    same edit and remove

--- script/sign-in.js
+++ script/sign-in.js
@@ -3,11 +3,9 @@ document.getElementById('signin-btn').addEventListener(('click'), () => {
   
     const inputName = document.getElementById('input-name');
      const userName = inputName.value;
-     console.log(userName)
 
     const inputPin = document.getElementById('input-pin');
      const pin = inputPin.value;
-     console.log(pin)
 
      if( userName == "admin" && pin == "admin123"){
         alert(" Sing In Successful");
